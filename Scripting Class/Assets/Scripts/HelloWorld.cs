5a4a0503f0d00bb781c26013ced884a0c2771a0d
Willie <buechelewd@gmail.com>
2019-01-09 10:29:15 -0700

    Reinit with gitignore
    Reinitiated the repository with the proper gitignore. Created a Unity project. created a basic script that printe "Hello World!" to the game console on game start. Imported basic art assets for a character. created a simple 2D platform with collider. Created a basic character with collider and rigidbody2D. added a simple scrip that allow you to scoot the character.

---
+++ Scripting Class/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//prints "Hello World" to the game console on game start
+		print("Hello World!");
+	}
+
+}
---
+++ Scripting Class/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody2D rb;
+
+	public float moveSpeed;
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			rb.velocity = new Vector2(-moveSpeed, 0);	
+		}
+
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			rb.velocity = new Vector2(moveSpeed, 0);
+		}
+	}
+}
+
