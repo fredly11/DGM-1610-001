8256d6f416a624913a79f7cac67a226501f2ec60
fredly11 <buechelewd@gmail.com>
2019-01-11 22:43:04 -0700

    Ground Check
    Reed movement script to work with an axis rather than key press. added a ground check to the movement script and adjusted it so the player can only jump when in contact with ground (of course... this includes being beside the ground as well for now...) also imported my rocks and large ground art.

--- Scripting Class/Assets/Scripts/PlayerController.cs
+++ Scripting Class/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	private Rigidbody2D rb;
-
+	public bool isGrounded;
 	public float moveSpeed;
-
 	public float jumpForce;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask groundLayer;
+	
+	
+	
 	// Use this for initialization
 	void Start ()
 	{
@@ -16,18 +21,21 @@ public class PlayerController : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey(KeyCode.A))
-		{
-			rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);	
-		}
+	void Update ()
+	{
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-		if (Input.GetKey(KeyCode.D))
+		if (Input.GetAxis("Horizontal") > 0)
 		{
-			rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+			rb.velocity = new Vector2(moveSpeed, rb.velocity.y);	
 		}
+		else if (Input.GetAxis("Horizontal") < 0)
+		{
+			rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+		}
+	
 
-		if (Input.GetKeyDown(KeyCode.W))
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
 		{
 			rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 		}
