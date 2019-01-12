b56a08e7346f2bdd2e156ad37f40f69bf18dae03
fredly11 <buechelewd@gmail.com>
2019-01-12 13:17:43 -0700

    Pressure Plate
    Created a Pressure Plate script that allows an object to trigger the movement of another object to a fixed point.

---
+++ Scripting Class/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+	public GameObject doorToOpen;
+	public Transform targetPoint;
+	public float openSpeed;
+	public bool isOpening = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isOpening)
+		{
+			if (doorToOpen.transform.position.y > targetPoint.position.y)
+			{
+				doorToOpen.transform.position = new Vector2(doorToOpen.transform.position.x, doorToOpen.transform.position.y - openSpeed);
+				if (doorToOpen.transform.position.y <= targetPoint.position.y)
+				{
+					doorToOpen.transform.position = new Vector2(doorToOpen.transform.position.x, targetPoint.position.y);
+					isOpening = false;
+				}
+			}
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		print("triggered");
+		if (other.tag == "Player")
+		{
+			isOpening = true;	
+		}
+	}
+}
