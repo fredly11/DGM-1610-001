c4869ec8916b24e846e99125d3d0209c97541ed9
Willie <buechelewd@gmail.com>
2019-01-16 12:10:55 -0700

    Powerup Spawner
    created a spawner that will create powerups and respawn them after a time (WIP)

---
+++ Cleo the Abberangel/Assets/Scripts/PowerupSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupSpawner : MonoBehaviour
+{
+	public GameObject powerupToSpawn;
+	public float respawnTime;
+	public float currentTime;
+	public bool isRespawning;
+
+	// Use this for initialization
+	void Start ()
+	{
+		SpawnPowerup();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isRespawning && Time.deltaTime > currentTime + respawnTime)
+		{
+			SpawnPowerup();
+			isRespawning = false;
+		}
+	}
+	
+	void SpawnPowerup()
+	{
+		Instantiate(powerupToSpawn, gameObject.transform);
+	}
+}
--- Cleo the Abberangel/Assets/Scripts/WingPowerup.cs
+++ Cleo the Abberangel/Assets/Scripts/WingPowerup.cs
@@ -1,21 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WingPowerup : MonoBehaviour
 {
-	public GameObject player;
-	public int jumpCount;
+	public GameObject spawner;
+	private GameObject player;
+	public int jumpCount = 5;
 
+	private PowerupSpawner ps;
 	private PlayerController pc;
 	// Use this for initialization
 	void Start () {
+		player = GameObject.Find("Cleo");
 		pc = player.GetComponent<PlayerController>();
+		ps = spawner.GetComponent<PowerupSpawner>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	
 	}
 
 	private void OnTriggerEnter2D(Collider2D other)
@@ -23,9 +28,11 @@ public class WingPowerup : MonoBehaviour
 
 		if (other.tag == "Player")
 		{
-
-			pc.jumpCount = 5;
+			
+			pc.jumpCount = jumpCount;
 			Destroy(this.gameObject);
+			ps.isRespawning = true;
+			ps.currentTime = Time.deltaTime;
 		}
 	}
 }
