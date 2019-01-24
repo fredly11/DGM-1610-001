53c0ade7e115122da164b0b702a5e3d0880f9089
Willie Buechele <buechelewd@gmail.com>
2019-01-24 11:32:17 -0700

    Moving Platform
    Made a script that moves a n object back and forth between two points.

---
+++ Cleo the Abberangel/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+	public Transform startPoint;
+	public Transform endPoint;
+
+	public float moveSpeed;
+
+	private bool moveTowards = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (moveTowards)
+		{
+			gameObject.transform.position = Vector3.MoveTowards(transform.position, endPoint.transform.position, moveSpeed);
+			if (transform.position == endPoint.transform.position) moveTowards = false;
+		}
+		else
+		{
+			gameObject.transform.position = Vector3.MoveTowards(transform.position, startPoint.transform.position, moveSpeed);
+			if (transform.position == startPoint.transform.position) moveTowards = true;
+		}
+
+		
+		
+	}
+}
