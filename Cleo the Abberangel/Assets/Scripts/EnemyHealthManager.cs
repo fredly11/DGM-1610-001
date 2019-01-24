ad8801b5cd9ddaf158fad280288c1ec73def9bdc
fredly11 <buechelewd@gmail.com>
2019-01-23 22:12:20 -0700

    Enemy Health Manager
    Created a health manager for an enemy

---
+++ Cleo the Abberangel/Assets/Scripts/EnemyHealthManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour
+{
+	public float maxHealth;
+
+	public float currentHealth;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void TakeDamage(float damageToTake)
+	{
+		currentHealth -= damageToTake;
+		if (currentHealth <= 0)
+		{
+			Die();
+		}
+	}
+
+	void Die()
+	{
+		
+	}
+}
