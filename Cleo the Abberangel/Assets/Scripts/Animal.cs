4fe522bf11d6f98c5c76f8527377487a4b5ba435
fredly11 <buechelewd@gmail.com>
2019-02-04 13:49:26 -0700

    Animal Classes

---
+++ Cleo the Abberangel/Assets/Scripts/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+	public int maxHealth;
+	public int currentHealth;
+	public float damage;
+	public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
