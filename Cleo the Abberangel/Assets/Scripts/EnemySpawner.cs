e73066230181e57c24616410d884f51f443a8be8
fredly11 <buechelewd@gmail.com>
2019-03-09 11:27:08 -0700

    Enemy Spawner

---
+++ Cleo the Abberangel/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemySpawner : MonoBehaviour
+{
+	public GameObject enemyToSpawn;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Spawn()
+	{
+		Instantiate(enemyToSpawn);
+	}
+}
