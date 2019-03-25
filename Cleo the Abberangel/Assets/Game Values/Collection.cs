ec932236afd64b01d43b38d677b09ac072fe9797
Willie <buechelewd@gmail.com>
2019-03-25 11:30:43 -0600

    Collection

---
+++ Cleo the Abberangel/Assets/Game Values/Collection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collection : ScriptableObject
+{
+
+    public List<Object> collection;
+
+
+    void addObject(Object item)
+    {
+        collection.Add(item);
+    }
+}
