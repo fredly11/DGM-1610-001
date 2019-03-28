9a05336002eeaf5e1631fa92eb5fee4c93805817
fredly11 <buechelewd@gmail.com>
2019-03-28 15:51:06 -0600

    New Door Script

---
+++ Cleo the Abberangel/Assets/Scripts/DoorMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour
+{
+	private Vector3 startPosition;
+	private Vector3 endPosition;
+	private bool moving;
+	public float moveDistance;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		startPosition = transform.position;
+		endPosition = startPosition;
+		endPosition.y -= moveDistance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (moving)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, endPosition,moveSpeed);
+		}
+	}
+}
